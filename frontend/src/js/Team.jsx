afac3a3f36985262bf5a3e35e14f34703be70bdd
hakomori64 <hakomori64@gmail.com>
2019-04-24 17:10:25 +0900

    fix #72, fix #73, fix #74, fix #75, fix #76, fix #77, fix #78, fix #79, fix #80, fix #81, fix #82, fix #83, fix #84, fix #85, fix #86, fix #87, fix #88, fix #89, fix #90, fix #91, fix #92, fix #93, fix #94, fix #95, fix #96, fix #97, fix #98, fix #99, fix #100, fix #101, fix #102, fix #103, fix #104, fix #105, fix #106, fix #107, fix #108, fix #109, fix #110, fix #111, fix #112, fix #113, fix #114, fix #115, fix #116, fix #117, fix #118, fix #119, fix #120

---
+++ frontend/src/js/Team.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Component } from 'react';
+
+
+class Team extends Component {
+
+  componentDidMount() {
+      this.fetchTeamData();
+  }
+
+  fetchTeamData = () => {
+      return fetch('http://localhost:8080/teams', {
+          method: 'GET',
+          credentials: "include",
+      }).then((response) => {
+          return response.json();
+      }).then((json) => {
+          json;
+      })
+  }
+
+  render() {
+    return (
+        <div>はろー</div>
+    );
+  }
+}
+
+export default Team;
