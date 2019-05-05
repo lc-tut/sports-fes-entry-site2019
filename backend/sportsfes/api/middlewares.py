9e7989ceaa6e5d275c7dea74bf884f70701b2c18
hakomori64 <hakomori64@gmail.com>
2019-05-05 22:22:20 +0900

    ignore entry date middleware for some request

--- backend/sportsfes/api/middlewares.py
+++ backend/sportsfes/api/middlewares.py
@@ -37,6 +37,12 @@ class EntryDateMiddleware(object):
         elif now > settings.ENTRY_DEADLINE_DATE and request.method == 'GET':
             return None
         
+        elif (
+            view_func.__name__ in ['token_signin_view', 'token_logout_view', 'is_registerable'] or
+            '/admin/' in request.path or
+            '/django-rq/' in request.path
+        ):
+            return None
         else:
             response = HttpResponse("outside the specified period", status=400)
             return response
