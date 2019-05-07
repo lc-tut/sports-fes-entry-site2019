7af44a4599385bce2b82011ad806abeabae86133
cl0wn <hakomori64@gmail.com>
2019-05-07 22:00:39 +0900

    Update settings.py

--- backend/sportsfes/sportsfes/settings.py
+++ backend/sportsfes/sportsfes/settings.py
@@ -180,7 +180,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+if STATIC_URL is None:
+    STATIC_URL = '/static/'
+    
 STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'static')
 
 MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'media')
